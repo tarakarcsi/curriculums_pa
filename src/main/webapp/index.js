ff30de21402c77f32d546a44286b034fee82122c
Károly Taraszovics <tarakarcsi94@gmail.com>
2019-06-30 10:53:08 +0200

    index.js extended with error handling functions

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -9,3 +9,65 @@ function showContents(ids) {
         }
     }
 }
+
+function newInfo(targetEl, message) {
+    newMessage(targetEl, 'info', message);
+}
+
+function newError(targetEl, message) {
+    newMessage(targetEl, 'error', message);
+}
+
+function newMessage(targetEl, cssClass, message) {
+    clearMessages();
+
+    const pEl = document.createElement('p');
+    pEl.classList.add('message');
+    pEl.classList.add(cssClass);
+    pEl.textContent = message;
+
+    targetEl.appendChild(pEl);
+}
+
+function clearMessages() {
+    const messageEls = document.getElementsByClassName('message');
+    for (let i = 0; i < messageEls.length; i++) {
+        const messageEl = messageEls[i];
+        messageEl.remove();
+    }
+}
+
+function removeAllChildren(el) {
+    while (el.firstChild) {
+        el.removeChild(el.firstChild);
+    }
+}
+
+function onNetworkError(response) {
+    document.body.remove();
+    const bodyEl = document.createElement('body');
+    document.appendChild(bodyEl);
+    newError(bodyEl, 'Network error, please try reloaing the page');
+}
+
+function onOtherResponse(targetEl, xhr) {
+    if (xhr.status === NOT_FOUND) {
+        newError(targetEl, 'Not found');
+        console.error(xhr);
+    } else {
+        const json = JSON.parse(xhr.responseText);
+        if (xhr.status === INTERNAL_SERVER_ERROR) {
+            newError(targetEl, `Server error: ${json.message}`);
+        } else if (xhr.status === UNAUTHORIZED || xhr.status === BAD_REQUEST) {
+            newError(targetEl, json.message);
+        } else {
+            newError(targetEl, `Unknown error: ${json.message}`);
+        }
+    }
+}
+
+function onLoad() {
+
+}
+
+document.addEventListener('DOMContentLoaded', onLoad);
